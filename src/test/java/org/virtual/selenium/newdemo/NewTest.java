94f913a93dceda626d0a6fe0995ec8f900046371
root <root@DESKTOP-V1J6ELD.localdomain>
2020-03-25 12:25:35 +0530

    six

--- src/test/java/org/virtual/selenium/newdemo/NewTest.java
+++ src/test/java/org/virtual/selenium/newdemo/NewTest.java
@@ -10,37 +10,21 @@ public class NewTest {
   @Test
   public void f()  {
 	 	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-	  	WebDriver driver=new ChromeDriver();
-	  	
-		 
-			driver.get("http://kushala.org/workspace/virtual-university/");
-			driver.manage().window().maximize();
-			 
-			
-			
-			WebElement name = driver.findElement(By.xpath("/html[1]/body[1]/div[4]/div[1]/input[1]"));
-			WebElement email = driver.findElement(By.xpath("/html[1]/body[1]/div[4]/div[1]/input[2]"));
-			WebElement subject = driver.findElement(By.xpath("//input[@placeholder='Subject']"));			
-			WebElement contact = driver.findElement(By.xpath("//input[@placeholder='Contact']"));
-			WebElement message = driver.findElement(By.xpath("//textarea[@placeholder='Enter your Message here']"));
-			
-			
-			
-//		    WebElement submit = driver.findElement(By.xpath("//textarea[@placeholder='Enter your Message here']"));
-			
-			name.sendKeys("simon");
-			email.sendKeys("abc@gmail.com");
-			subject.sendKeys("nothing");
-			contact.sendKeys("12345");
-			message.sendKeys("hello there");
-//			submit.click();
-			
+		ChromeOptions chromeOptions = new ChromeOptions();
+	                chromeOptions.addArguments("--headless");
+		                chromeOptions.addArguments("--no-sandbox");
+
+			                WebDriver driver = new ChromeDriver(chromeOptions);
+
+				                driver.get("https://google.com");
+
+					                Thread.sleep(1000);
+
+						                if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+								                        System.out.println("Pass");
+										                } else {
+												                        System.out.println("Fail");
+														                }
+							                driver.quit();	
   }
 }
